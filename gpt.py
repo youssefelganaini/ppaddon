375c5c4e9122b67b6f809f92ca1f1ea3355ca2bb
youssefelganaini <youssef.elganaini@tum.de>
2023-11-20 21:39:41 +0100

    new changes to prompt, functionality works, prompt needs some work

--- gpt.py
+++ gpt.py
@@ -25,17 +25,40 @@ openai.api_key = OPENAI_KEY
 
 def send_gpt(message_gpt):
 
-    message_gpt += """
-        ---
-        
-        Das ist eine wichtige Präsentation, die ich gerade für einen relevanten Kunden mit hohen Anforderungen erstelle.
-        In der Präsentation dürfen keine Fehler vorkommen. Überprüfe daher jede Folie sorgfältig auf Rechtschreib- und Grammatikfehler.
-        Zudem ist es wichtig, dass die einzelnen Text einheitlich formuliert sind. Auch wenn mehrere Bullet-Points inhaltich klar ist, soll zudem garantiert sein, dass diese stilitisch ganz einheitlich formuliert sind.
-        Achte zudem darauf, dass keine Abkürzungen verwendet werden, die nicht durch Ziffern in Form von Fußnoten näher erklärt werden.
-        Bindestriche kannst du vernachlässigen, da diese den Zweck haben, ein Wort über mehrere Zeilen zu erstrecken und somit keine ungewollten Fehler sind.
-        Erstelle eine Tabelle, in der du die Fehler für jede Folie nummeriert aufgeführt werden. Zu jedem Fehler sollte ein Verbesserungsvorschlag angegeben werden.
-        Falls du inkonsistene Formulierung gefunden hast, gibt unbedingt Beispiele dafür und Vorschläge, wie du es verbessern würdest.
-        """
+    action_title_criteria = """
+    Das sind Kriterien für die Action Titles mit jeweils einem Beispiel für das Kriterium
+    1. Kriterium: Vermeidet generelle Aussagen, die immer richtig oder banal sind​
+    Beispiel: Als IT-Spezialist übernimmt Florian die Systemanbindung und technische Implementierung
+    2. Kriterium: Vermeidet absolute oder sinnlose Statements, die leicht falsifiziert werden können​
+    Beispiel: Die Strategie wird durch unbefangene Hypothesen, Kundeninterviews und einen Unternehmensworkshop entwickelt
+    3. Kriterium: Fokussiert euch auf Ergebnisse, nicht auf den Prozess​
+    Beispiel: Die Analyse des Status Quo dient als Grundlage zur weiteren zielgerichteten Durchführung des Projekts 
+    ​4. Kriterium: Quantifiziert so oft wie möglich die Ergebnisse​
+    80(%) der Alumni sind mit der Vernetzung im Verein unzufrieden, wobei sich 60(%) ein 1:1-Buddy-Komzept und 40% mehr Events wünschen 
+    5. Kriterium: Schreibt präzise Action Title, idealerweise nur 1 Zeile​
+    Bis spätestens Ende März wird das Projekt abgeschlossen 
+    """
+
+    message_gpt += f"""
+    Das ist eine wichtige Präsentation, die ich gerade für einen relevanten Kunden mit hohen Anforderungen erstelle.
+    In der Präsentation dürfen keine Fehler vorkommen. Überprüfe daher jede Folie sorgfältig auf Rechtschreib- und Grammatikfehler.
+    Überprüfe die Action Titles auf folgende Kriterien:
+    {action_title_criteria}
+    Gib mir eine Rückmeldung in dieser Form:
+    (Foliennummer): (Action Title) 
+    (Feedback zum Action Title basierend auf Kriterien)
+    (Vorgeschlagene Action Titles)
+    """
+    # message_gpt += """
+    #     ---
+    #     Das ist eine wichtige Präsentation, die ich gerade für einen relevanten Kunden mit hohen Anforderungen erstelle.
+    #     In der Präsentation dürfen keine Fehler vorkommen. Überprüfe daher jede Folie sorgfältig auf Rechtschreib- und Grammatikfehler.
+    #     Zudem ist es wichtig, dass die einzelnen Text einheitlich formuliert sind. Auch wenn mehrere Bullet-Points inhaltich klar ist, soll zudem garantiert sein, dass diese stilitisch ganz einheitlich formuliert sind.
+    #     Achte zudem darauf, dass keine Abkürzungen verwendet werden, die nicht durch Ziffern in Form von Fußnoten näher erklärt werden.
+    #     Bindestriche kannst du vernachlässigen, da diese den Zweck haben, ein Wort über mehrere Zeilen zu erstrecken und somit keine ungewollten Fehler sind.
+    #     Erstelle eine Tabelle, in der du die Fehler für jede Folie nummeriert aufgeführt werden. Zu jedem Fehler sollte ein Verbesserungsvorschlag angegeben werden.
+    #     Falls du inkonsistene Formulierung gefunden hast, gibt unbedingt Beispiele dafür und Vorschläge, wie du es verbessern würdest.
+    #     """
 
     response = openai.ChatCompletion.create(
         model="gpt-4",
--- main.py
+++ main.py
@@ -4,53 +4,39 @@ from gpt import send_gpt
 pptx_path = "./Abschlusspräsentation Schalke hilft!.pptx"
 output_path = "./response.txt"
 
+text_dict = {}  # dict that maps slide number to action title
+
 
 def extract_text_from_pptx(pptx_path):
     # Load the presentation
     prs = Presentation(pptx_path)
 
     # Iterate through slides, then through slide elements to extract text
-    full_text = []
     count_slide = 0
     for slide in prs.slides:
         count_slide += 1
-        full_text.append([])
-        full_text[-1].append("//Folie " + str(count_slide) + "\n")
-
-        count_box = 0
+        # add another dict key
+        text_dict[count_slide] = None
 
         # Get Action title
         if slide.shapes.title is not None:
-            full_text[-1].append(
-                "BOX " + str(count_box) + "\n" + slide.shapes.title.text
-            )
-            full_text[-1].append(slide.shapes.title.text)
+            # add slide action title
+            text_dict[count_slide] = slide.shapes.title.text
 
-        # for shape in slide.shapes:
-        #     if hasattr(shape, "text") and full_text is None:
-        #         # count_box += 1
-        #         # full_text[-1].append("BOX " + str(count_box) + "\n" + shape.text)
-        #         print(full_text)
-        #         full_text[-1].append(shape.text)
-
-    return full_text
+    return text_dict
 
 
+# save output of chatgpt return value
 def save_to_txt_file(text, output_path):
     with open(output_path, "w") as file:
         file.write(text)
 
 
+# Add string for ChatGPT prompt
+gpt_string = ""
 text = extract_text_from_pptx(pptx_path)
+for elem in text:
+    gpt_string += "Folie " + str(elem) + ": " + str(text[elem]) + "\n"
 
-result = ""
-for slide in text:
-    for box in slide:
-        result += box + "\n"
-    result += "\n"
-
-with open("bsp.txt", "w") as file:
-    file.write(result)
-
-# response = send_gpt(result)
-# save_to_txt_file(response["choices"][0]["message"]["content"], output_path)
+response = send_gpt(gpt_string)
+save_to_txt_file(response["choices"][0]["message"]["content"], output_path)
