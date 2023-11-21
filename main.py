8cd4a3e8f7da218cb87c7f630b63e0d7101517d7
youssefelganaini <youssef.elganaini@tum.de>
2023-11-21 09:41:25 +0100

    minor adjustments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pptx import Presentation
 from gpt import send_gpt
 
-pptx_path = "./Abschlusspräsentation Schalke hilft!.pptx"
-output_path = "./response.txt"
+PPTX_PATH = "./Abschlusspräsentation Schalke hilft!.pptx"
+OUTPUT_PATH = "./response.txt"
 
 text_dict = {}  # dict that maps slide number to action title
 
@@ -20,10 +20,17 @@ def extract_text_from_pptx(pptx_path):
 
         # Get Action title
         if slide.shapes.title is not None:
-            # add slide action title
+            # add slide action title to dict key
             text_dict[count_slide] = slide.shapes.title.text
 
     return text_dict
+
+
+# Add string for ChatGPT prompt
+gpt_string = ""
+text = extract_text_from_pptx(PPTX_PATH)
+for elem in text:
+    gpt_string += "Folie " + str(elem) + ": " + str(text[elem]) + "\n"
 
 
 # save output of chatgpt return value
@@ -32,11 +39,5 @@ def save_to_txt_file(text, output_path):
         file.write(text)
 
 
-# Add string for ChatGPT prompt
-gpt_string = ""
-text = extract_text_from_pptx(pptx_path)
-for elem in text:
-    gpt_string += "Folie " + str(elem) + ": " + str(text[elem]) + "\n"
-
 response = send_gpt(gpt_string)
-save_to_txt_file(response["choices"][0]["message"]["content"], output_path)
+save_to_txt_file(response["choices"][0]["message"]["content"], OUTPUT_PATH)
